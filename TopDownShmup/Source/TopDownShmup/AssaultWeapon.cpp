bcbf857160b7df774a36a5df77e73fb591dda662
obinnakalu135 <obinnakalu135@gmail.com>
2023-03-24 20:01:05 -0700

    Commit

---
+++ TopDownShmup/Source/TopDownShmup/AssaultWeapon.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssaultWeapon.h"
+
+void AAssaultWeapon::OnStartFire() {
+    Super::OnStartFire();
+}
+
+
+void AAssaultWeapon::OnStopFire() {
+    Super::OnStopFire();
+}
